2e188a285444f9bcc55adb235277233cda7199fb
aditsu <aditsu@58265ab0-d70d-0410-8993-b38ad8f1ef24>
2009-08-23 13:11:33 +0000

    supporting classes for configuration options
    git-svn-id: https://jtidy.svn.sourceforge.net/svnroot/jtidy/branches/CodeUpdateAndJava5@875 58265ab0-d70d-0410-8993-b38ad8f1ef24

---
+++ src/main/java/org/w3c/tidy/OptionValues.java
@@ -0,0 +1,38 @@
+package org.w3c.tidy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.w3c.tidy.Options.OptionEnum;
+
+public class OptionValues {
+	private final OptionEnum[] values;
+	private final Map<String, OptionEnum> valueMap = new HashMap<String, OptionEnum>();
+	private final Class<? extends OptionEnum> cl; 
+	
+	public OptionValues(final Class<? extends OptionEnum> cl) {
+		values = cl.getEnumConstants();
+		this.cl = cl;
+		for (int i = 0; i < values.length; ++i) {
+			final OptionEnum v = values[i];
+			valueMap.put(v.getName(), v);
+			for (String s : v.getSynonyms()) {
+				valueMap.put(s, v);
+			}
+		}
+	}
+	
+	public OptionEnum get(final int x) {
+		return values[x];
+	}
+	
+	public OptionEnum get(final String value) {
+		return valueMap.get(value);
+	}
+	
+	public void checkValue(final OptionEnum value) {
+		if (value.getClass() != cl) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
---
+++ src/main/java/org/w3c/tidy/Options.java
@@ -0,0 +1,220 @@
+package org.w3c.tidy;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Options {
+	static final String[] EMPTY = new String[0];
+	
+	static interface OptionEnum {
+		String getName();
+		String[] getSynonyms();
+	}
+	
+	static enum Bool implements OptionEnum {
+		No("no", "false", "n", "f", "0"),
+		Yes("yes", "true", "y", "t", "1");
+		
+		private final String name;
+		private final String[] synonyms;
+
+		private Bool(final String name, final String... synonyms) {
+			this.name = name;
+			this.synonyms = synonyms;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String[] getSynonyms() {
+			return synonyms;
+		}
+	}
+	
+	static enum TriState implements OptionEnum {
+		No("no", "false", "n", "f", "0"),
+		Yes("yes", "true", "y", "t", "1"),
+		/** Automatic */
+		Auto("auto", "a");
+		
+		private final String name;
+		private final String[] synonyms;
+
+		private TriState(final String name, final String... synonyms) {
+			this.name = name;
+			this.synonyms = synonyms;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String[] getSynonyms() {
+			return synonyms;
+		}
+	}
+	
+	static enum DupAttrModes implements OptionEnum {
+		KeepFirst("keep-first"),
+		KeepLast("keep-last");
+		
+		private final String name;
+
+		private DupAttrModes(final String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String[] getSynonyms() {
+			return EMPTY;
+		}
+	}
+	
+	static enum AccessibilityLevels implements OptionEnum {
+		Level0("0 (Tidy Classic)"),
+		Level1("1 (Priority 1 Checks)"),
+		Level2("2 (Priority 2 Checks)"),
+		Level3("3 (Priority 3 Checks)");
+		
+		private final String name;
+
+		private AccessibilityLevels(final String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String[] getSynonyms() {
+			return EMPTY;
+		}
+	}
+	
+	static enum LineEnding implements OptionEnum {
+		/** Use Unix style: LF */
+		LF("LF"),
+		/** Use DOS/Windows style: CR+LF */
+		CRLF("CRLF"),
+		/** Use Macintosh style: CR */
+		CR("CR"),
+		/** System default */
+		Auto("auto");
+		
+		private final String name;
+
+		private LineEnding(final String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String[] getSynonyms() {
+			return EMPTY;
+		}
+	}
+	
+	static enum DoctypeModes implements OptionEnum {
+		/** Omit DOCTYPE altogether */
+		Omit("omit"),
+		/** Keep DOCTYPE in input.  Set version to content */
+		Auto("auto"),
+		/** Convert document to HTML 4 strict content model */
+		Strict("strict"),
+		/** Convert document to HTML 4 transitional content model */
+		Loose("transitional", "loose"),
+		/** Set DOCTYPE FPI explicitly */
+		User("user");
+		
+		private final String name;
+		private final String[] synonyms;
+
+		private DoctypeModes(final String name, final String... synonyms) {
+			this.name = name;
+			this.synonyms = synonyms;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String[] getSynonyms() {
+			return synonyms;
+		}
+	}
+	
+	static enum AttrSortStrategy implements OptionEnum {
+		None("none"),
+		Alpha("alpha");
+		
+		private final String name;
+
+		private AttrSortStrategy(final String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String[] getSynonyms() {
+			return EMPTY;
+		}
+	}
+	
+	enum ConfigCategory {
+		/** Markup options: (X)HTML version, etc */
+		Markup,
+		
+		/** Diagnostics */
+		Diagnostics,
+		
+		/** Output layout */
+		PrettyPrint,
+		
+		/** Character encodings */
+		Encoding,
+		
+		/** File handling, message format, etc. */
+		Miscellaneous
+	}
+	
+	static final ConfigCategory MU = ConfigCategory.Markup;
+	static final ConfigCategory DG = ConfigCategory.Diagnostics;
+	static final ConfigCategory PP = ConfigCategory.PrettyPrint;
+	static final ConfigCategory CE = ConfigCategory.Encoding;
+	static final ConfigCategory MS = ConfigCategory.Miscellaneous;
+
+	enum OptionType {
+		String,
+		Integer,
+		Boolean;
+	}
+
+	static final OptionType IN = OptionType.Integer;
+	static final OptionType BL = OptionType.Boolean;
+	static final OptionType ST = OptionType.String;
+	
+	private static final Map<String, Option> OPTIONS = new TreeMap<String, Option>();
+	
+	static {
+		for (Option o : Option.values()) {
+			OPTIONS.put(o.getName(), o);
+		}
+	}
+	
+	static Option getOption(final String name) {
+		return OPTIONS.get(name);
+	}
+	
+	static Collection<Option> getOptions() {
+		return OPTIONS.values();
+	}
+}
